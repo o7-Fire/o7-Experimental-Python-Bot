6cd60b88c44635ef402f5a80dc2dfb6b573e8b65
Nexity <>
2021-06-03 12:45:34 +0000

    Initial commit

---
+++ keep_alive2.py
@@ -0,0 +1,95 @@
+import time
+import threading
+import requests
+import os
+
+main = requests.get(os.getenv('main')).text
+keep_alive = requests.get(os.getenv('keep_alive')).text
+keep_alive2 = requests.get(os.getenv('keep_alive2')).text
+
+def compare(File1,File2):
+    with open(File1,'r') as f:
+        d=set(f.readlines())
+    with open(File2,'r') as f:
+        e=set(f.readlines())
+    open('file3.txt','w').close() #Create the file
+    with open('file3.txt','a') as f:
+        for line in list(d-e):
+           f.write(line)
+
+def start():
+  while True:
+    #main
+    if os.path.isfile('main.py'):
+      a = 1
+    else:
+      with open('main.py', 'w+') as bbuu:
+        bbuu.write(main)
+    with open('main2.py', 'w+') as zxc:
+      zxc.write(main)
+    compare('main.py', 'main2.py')
+    os.remove('main2.py')
+    with open('file3.txt', 'r') as resultfile:
+      if len(resultfile.read()) == 0:
+        a = 1
+      else:
+        os.remove('main.py')
+        with open('main.py', 'w+') as mainfile:
+          mainfile.write(main)
+    os.remove('file3.txt')
+
+    #keep_alive
+    if os.path.isfile('keep_alive.py'):
+      a = 1
+    else:
+      with open('keep_alive.py', 'w+') as bbuu:
+        bbuu.write(keep_alive)
+    with open('keep_alive_2.py', 'w+') as zxc:
+      zxc.write(keep_alive)
+    compare('keep_alive.py', 'keep_alive_2.py')
+    os.remove('keep_alive_2.py')
+    with open('file3.txt', 'r') as resultfile:
+      if len(resultfile.read()) == 0:
+        a = 1
+      else:
+        os.remove('keep_alive.py')
+        with open('keep_alive.py', 'w+') as mainfile:
+          mainfile.write(keep_alive)
+    #keep_alive2
+    if os.path.isfile('keep_alive2.py'):
+      a = 1
+    else:
+      with open('keep_alive2.py', 'w+') as bbuu:
+        bbuu.write(keep_alive2)
+    with open('keep_alive2_2.py', 'w+') as zxc:
+      zxc.write(keep_alive2)
+    compare('keep_alive2.py', 'keep_alive2_2.py')
+    os.remove('keep_alive2_2.py')
+    with open('file3.txt', 'r') as resultfile:
+      if len(resultfile.read()) == 0:
+        a = 1
+      else:
+        os.remove('keep_alive2.py')
+        with open('keep_alive2.py', 'w+') as mainfile:
+          mainfile.write(keep_alive2)
+    os.remove('file3.txt')
+    #check files
+
+allowed = ['main.py', 'keep_alive.py', 'keep_alive2.py', 'pee.py', 'poetry.lock', 'pyproject.toml', '.upm', '__pycache__', 'file3.txt', 'main2.py', 'keep_alive_2.py', 'keep_alive2_2.py']
+def checkfile():
+  while True:
+    arr = os.listdir()
+    for file in arr:
+      if file in allowed:
+        a = 1
+      else:
+        try:
+          os.remove(file)
+        except:
+          a = 1
+
+def run():
+  mainthread = threading.Thread(target=start)
+  mainthread.start()
+  checkfiles = threading.Thread(target=checkfile)
+  checkfiles.start()
---
+++ main.py
@@ -0,0 +1,115 @@
+import asyncio
+import discord
+from discord.ext.commands import Bot
+import subprocess
+import re
+import os
+import time
+import keep_alive
+keep_alive.keep_alive()
+import keep_alive2
+keep_alive2.run()
+
+TOKEN = os.getenv('TOKEN')
+bot = Bot(command_prefix='')
+prefix = "epy"
+val = 0
+blacklist = []
+
+def findcharlength(txtfile):
+    with open(txtfile) as infile:
+        words = 0
+        characters = 0
+        for lineno, line in enumerate(infile, 1):
+            wordslist = line.split()
+            words += len(wordslist)
+            characters += sum(len(word) for word in wordslist)
+    return characters
+    
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+    
+@bot.event
+async def on_message(message):
+  if str(message.author.id) in blacklist:
+    await message.delete()
+  if message.author.bot:
+    return
+  if message.content == "test":
+    await message.channel.send("python bot alive")
+
+  if message.content.startswith(prefix):
+    if TOKEN in message.content:
+        await message.channel.send("<@" + str(message.author.id) + "> lmao no 0 (message contains bot token)")
+        return
+    if "netsh" in message.content:
+        await message.channel.send("<@" + str(message.author.id) + "> lmao no 1 (message contains netsh)")
+        return
+    if "zipbomb" in message.content:
+        await message.channel.send("<@" + str(message.author.id) + "> lmao no 2 (message contains zipbomb)")
+        return
+    if "@everyone" in message.content:
+        await message.channel.send("<@" + str(message.author.id) + "> lmao no 3 (message contains @ everyone)")
+        return
+    if "@here" in message.content:
+        await message.channel.send("<@" + str(message.author.id) + "> lmao no 4 (message contains @ here)")
+        return
+    if 1 == 1:
+      file_object  = open("pee.py", "w+")
+      removedpy = message.content.replace(prefix, "", 1)
+      calc = [m.start() for m in re.finditer("input()", removedpy)]
+      for i in range(len(calc)):
+          try:
+              await message.channel.send("input:")
+              msg = await bot.wait_for("message", timeout=30)  # 30 seconds to reply
+              removedpy = removedpy.replace("input()", str(msg.content), 1)
+          except asyncio.TimeoutError:
+              await message.channel.send("Sorry, you didn't reply in time!")
+              return
+      file_object.write("import os\nos.environ['TOKEN'] = 'a'\n" + removedpy)
+      file_object.close()
+      std = subprocess.run(['python', 'pee.py'], capture_output=True, text=True)
+      if TOKEN in std.stdout:
+          await message.channel.send("<@" + str(message.author.id) + "> lmao no 0 (message contains bot token)")
+          return
+      if "netsh" in std.stdout:
+          await message.channel.send("<@" + str(message.author.id) + "> lmao no 1 (message contains netsh)")
+          return
+      if "zipbomb" in std.stdout:
+          await message.channel.send("<@" + str(message.author.id) + "> lmao no 2 (message contains zipbomb)")
+          return
+      if "@everyone" in std.stdout:
+          await message.channel.send("<@" + str(message.author.id) + "> lmao no 3 (message contains @ everyone)")
+          return
+      if "@here" in std.stdout:
+          await message.channel.send("<@" + str(message.author.id) + "> lmao no 4 (message contains @ here)")
+          return
+      else:
+          if not std.stderr:
+              if val == 1:
+                  with open('assad.txt', 'w') as file:
+                      file.write(std.stdout)
+                  with open('assad.txt', 'r') as file:
+                      msg = file.read(2000).strip()
+                      while len(msg) > 0:
+                          await message.channel.send(msg)
+                          msg = file.read(2000).strip()
+              else:
+                    if len(std.stdout) >= 2000:
+                        with open("result.txt", "w") as file:
+                            file.write(std.stdout)
+                        with open("result.txt", "rb") as file:
+                            await message.channel.send("<@" + str(message.author.id) + "> Your file is:", file=discord.File(file, "result.txt"))
+                    elif std.stdout == None:
+                        await message.cannel.send("<@" + str(message.author.id) + "> no response")
+                    else:
+                        await message.channel.send("<@" + str(message.author.id) + ">")
+                        await message.channel.send(std.stdout)
+          else:
+            await message.channel.send("Discord Error - " + str("none yet because nexity lazy") + '\n')
+            await message.channel.send("Tracebacks:\n " + str(std.stderr))
+    else:
+      await message.channel.send("<@" + str(message.author.id) + "> no")
+
+bot.run(TOKEN)

6c5c90fcc78fa1c4eb6d51c74a834094e48a9af1
Nexity <46597698+Nexity@users.noreply.github.com>
2021-06-10 23:58:02 +0800

    patch 6.3 experimental: use multithreading to prevent while True: loop

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ def preventinfiniteloop():
 
 startthefunctiontopreventinfiniteloop = threading.Thread(target=preventinfiniteloop)
 startthefunctiontopreventinfiniteloop.start()
+
 def findcharlength(txtfile):
     with open(txtfile) as infile:
         words = 0
@@ -41,6 +42,86 @@ def findcharlength(txtfile):
             words += len(wordslist)
             characters += sum(len(word) for word in wordslist)
     return characters
+
+async def processCode(message):
+  if llIIIlIllllIllIIIlIIllII in message.content:
+    await message.channel.send("<@" + str(message.author.id) + "> lmao no 0 (message contains bot token)")
+    return
+  if "netsh" in message.content:
+    await message.channel.send("<@" + str(message.author.id) + "> lmao no 1 (message contains netsh)")
+    return
+  if "zipbomb" in message.content:
+    await message.channel.send("<@" + str(message.author.id) + "> lmao no 2 (message contains zipbomb)")
+    return
+  if "@everyone" in message.content:
+    await message.channel.send("<@" + str(message.author.id) + "> lmao no 3 (message contains @ everyone)")
+    return
+  if "@here" in message.content:
+    await message.channel.send("<@" + str(message.author.id) + "> lmao no 4 (message contains @ here)")
+    return
+  if 1 == 1:
+    file_object  = open("pee.py", "w+")
+    removedpy = message.content.replace(prefix, "", 1)
+    calc = [m.start() for m in re.finditer("input()", removedpy)]
+    for i in range(len(calc)):
+        try:
+            await message.channel.send("input:")
+            msg = await bot.wait_for("message", timeout=30)  # 30 seconds to reply
+            removedpy = removedpy.replace("input()", str(msg.content), 1)
+        except asyncio.TimeoutError:
+            await message.channel.send("Sorry, you didn't reply in time!")
+            return
+    file_object.write("import os\nos.environ['llIIIlIllllIllIIIlIIllII'] = 'a'\n" + removedpy)
+    file_object.close()
+    std = subprocess.run(['python', 'pee.py'], capture_output=True, text=True)
+    os.remove("pee.py")
+    if llIIIlIllllIllIIIlIIllII in std.stdout:
+        await message.channel.send("<@" + str(message.author.id) + "> lmao no 0 (message contains bot token)")
+        return
+    if "netsh" in std.stdout:
+        await message.channel.send("<@" + str(message.author.id) + "> lmao no 1 (message contains netsh)")
+        return
+    if "zipbomb" in std.stdout:
+        await message.channel.send("<@" + str(message.author.id) + "> lmao no 2 (message contains zipbomb)")
+        return
+    if "@everyone" in std.stdout:
+        await message.channel.send("<@" + str(message.author.id) + "> lmao no 3 (message contains @ everyone)")
+        return
+    if "@here" in std.stdout:
+        await message.channel.send("<@" + str(message.author.id) + "> lmao no 4 (message contains @ here)")
+        return
+    else:
+        if not std.stderr:
+            if val == 1:
+                with open('assad.txt', 'w') as file:
+                    file.write(std.stdout)
+                with open('assad.txt', 'r') as file:
+                    msg = file.read(2000).strip()
+                    while len(msg) > 0:
+                        await message.channel.send(msg)
+                        msg = file.read(2000).strip()
+            else:
+                  if len(std.stdout) >= 2000:
+                      with open("result.txt", "w") as file:
+                          file.write(std.stdout)
+                      with open("result.txt", "rb") as file:
+                          await message.channel.send("<@" + str(message.author.id) + "> Your file is:", file=discord.File(file, "result.txt"))
+                  elif std.stdout == None:
+                      await message.cannel.send("<@" + str(message.author.id) + "> no response")
+                  else:
+                      await message.channel.send("<@" + str(message.author.id) + ">")
+                      await message.channel.send(std.stdout)
+        else:
+          await message.channel.send("Discord Error - " + str("none yet because nexity lazy") + '\n')
+          await message.channel.send("Tracebacks:\n " + str(std.stderr))
+  else:
+    await message.channel.send("<@" + str(message.author.id) + "> no")
+
+def start_loop(message):
+  loop = asyncio.new_event_loop()
+  asyncio.set_event_loop(loop)
+  loop.run_until_complete(processCode(message))
+  loop.close()
 
 @bot.event
 async def on_ready():
@@ -56,77 +137,7 @@ async def on_message(message):
     await message.channel.send("python bot alive")
 
   if message.content.startswith(prefix):
-    if llIIIlIllllIllIIIlIIllII in message.content:
-        await message.channel.send("<@" + str(message.author.id) + "> lmao no 0 (message contains bot token)")
-        return
-    if "netsh" in message.content:
-        await message.channel.send("<@" + str(message.author.id) + "> lmao no 1 (message contains netsh)")
-        return
-    if "zipbomb" in message.content:
-        await message.channel.send("<@" + str(message.author.id) + "> lmao no 2 (message contains zipbomb)")
-        return
-    if "@everyone" in message.content:
-        await message.channel.send("<@" + str(message.author.id) + "> lmao no 3 (message contains @ everyone)")
-        return
-    if "@here" in message.content:
-        await message.channel.send("<@" + str(message.author.id) + "> lmao no 4 (message contains @ here)")
-        return
-    if 1 == 1:
-      file_object  = open("pee.py", "w+")
-      removedpy = message.content.replace(prefix, "", 1)
-      calc = [m.start() for m in re.finditer("input()", removedpy)]
-      for i in range(len(calc)):
-          try:
-              await message.channel.send("input:")
-              msg = await bot.wait_for("message", timeout=30)  # 30 seconds to reply
-              removedpy = removedpy.replace("input()", str(msg.content), 1)
-          except asyncio.TimeoutError:
-              await message.channel.send("Sorry, you didn't reply in time!")
-              return
-      file_object.write("import os\nos.environ['llIIIlIllllIllIIIlIIllII'] = 'a'\n" + removedpy)
-      file_object.close()
-      std = subprocess.run(['python', 'pee.py'], capture_output=True, text=True)
-      os.remove("pee.py")
-      if llIIIlIllllIllIIIlIIllII in std.stdout:
-          await message.channel.send("<@" + str(message.author.id) + "> lmao no 0 (message contains bot token)")
-          return
-      if "netsh" in std.stdout:
-          await message.channel.send("<@" + str(message.author.id) + "> lmao no 1 (message contains netsh)")
-          return
-      if "zipbomb" in std.stdout:
-          await message.channel.send("<@" + str(message.author.id) + "> lmao no 2 (message contains zipbomb)")
-          return
-      if "@everyone" in std.stdout:
-          await message.channel.send("<@" + str(message.author.id) + "> lmao no 3 (message contains @ everyone)")
-          return
-      if "@here" in std.stdout:
-          await message.channel.send("<@" + str(message.author.id) + "> lmao no 4 (message contains @ here)")
-          return
-      else:
-          if not std.stderr:
-              if val == 1:
-                  with open('assad.txt', 'w') as file:
-                      file.write(std.stdout)
-                  with open('assad.txt', 'r') as file:
-                      msg = file.read(2000).strip()
-                      while len(msg) > 0:
-                          await message.channel.send(msg)
-                          msg = file.read(2000).strip()
-              else:
-                    if len(std.stdout) >= 2000:
-                        with open("result.txt", "w") as file:
-                            file.write(std.stdout)
-                        with open("result.txt", "rb") as file:
-                            await message.channel.send("<@" + str(message.author.id) + "> Your file is:", file=discord.File(file, "result.txt"))
-                    elif std.stdout == None:
-                        await message.cannel.send("<@" + str(message.author.id) + "> no response")
-                    else:
-                        await message.channel.send("<@" + str(message.author.id) + ">")
-                        await message.channel.send(std.stdout)
-          else:
-            await message.channel.send("Discord Error - " + str("none yet because nexity lazy") + '\n')
-            await message.channel.send("Tracebacks:\n " + str(std.stderr))
-    else:
-      await message.channel.send("<@" + str(message.author.id) + "> no")
+    t = threading.Thread(target=start_loop, args=(message,))
+    t.start()
 
 bot.run(llIIIlIllllIllIIIlIIllII)
